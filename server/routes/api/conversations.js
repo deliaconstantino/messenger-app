9190005c212e38bddc05b7f5e1aa311afe4a7a96
deliaconstantino <dconstan92@gmail.com>
2021-07-17 13:43:37 -0400

    reverted Sequelize call for Message back to order by 'createdAt'

--- server/routes/api/conversations.js
+++ server/routes/api/conversations.js
@@ -20,7 +20,7 @@ router.get("/", async (req, res, next) => {
         },
       },
       attributes: ["id"],
-      order: [[Message, "updatedAt", "DESC"]],
+      order: [[Message, "createdAt", "DESC"]],
       include: [
         { model: Message, order: ["createdAt", "ASC"] },
         {
