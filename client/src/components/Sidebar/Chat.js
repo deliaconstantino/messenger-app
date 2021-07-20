d5681b5e441740547bd78da87ff4953a108108b0
deliaconstantino <dconstan92@gmail.com>
2021-07-19 22:45:36 -0400

    updated spacing and removed comments

--- client/src/components/Sidebar/Chat.js
+++ client/src/components/Sidebar/Chat.js
@@ -4,8 +4,8 @@ import { BadgeAvatar, ChatContent } from "../Sidebar";
 import { withStyles } from "@material-ui/core/styles";
 import { setActiveChat } from "../../store/activeConversation";
 import { connect } from "react-redux";
-import { updateReadStatus } from "../../store/utils/thunkCreators"
-import BadgeUnread from "./BadgeUnread"
+import { updateReadStatus } from "../../store/utils/thunkCreators";
+import BadgeUnread from "./BadgeUnread";
 
 const styles = {
   root: {
@@ -24,15 +24,13 @@ const styles = {
 class Chat extends Component {
   handleClick = async (conversation) => {
     await this.props.setActiveChat(conversation.otherUser.username);
-
     const reqBody = {
-      conversationId: this.props.conversation.id
-    }
+      conversationId: this.props.conversation.id,
+    };
     await this.props.updateReadStatus(reqBody);
   };
 
   render() {
-    console.log("props from chat", this.props)
     const { classes } = this.props;
     const otherUser = this.props.conversation.otherUser;
     const count = this.props.conversation.unreadMessagesCount;
@@ -48,7 +46,7 @@ class Chat extends Component {
           sidebar={true}
         />
         <ChatContent conversation={this.props.conversation} />
-        {count > 0 ? <BadgeUnread count={count}/> : null}
+        {count > 0 ? <BadgeUnread count={count} /> : null}
       </Box>
     );
   }
@@ -61,7 +59,7 @@ const mapDispatchToProps = (dispatch) => {
     },
     updateReadStatus: (conversationId) => {
       dispatch(updateReadStatus(conversationId));
-    }
+    },
   };
 };
 
