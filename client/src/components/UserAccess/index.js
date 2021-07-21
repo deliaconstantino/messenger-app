3ebfb1e519b9ba5487eec49021d267aa27db4448
deliaconstantino <dconstan92@gmail.com>
2021-07-20 21:40:27 -0400

    removed UserAccess component

--- client/src/components/UserAccess/index.js
+++ client/src/components/UserAccess/index.js
@@ -1,4 +1,3 @@
-export { default as UserAccess } from "./UserAccess";
 export { default as Login } from "./Login";
 export { default as Signup } from "./Signup";
 export { default as UserAccessTwo } from "./UserAccessTwo";
