09cfb0ce2aab4732fb67df82b80447f954407145
deliaconstantino <dconstan92@gmail.com>
2021-07-21 18:01:03 -0400

    render SeenAvatar under either chat Bubble

--- client/src/components/ActiveChat/SeenAvatar.js
+++ client/src/components/ActiveChat/SeenAvatar.js
@@ -3,10 +3,12 @@ import { Box, Avatar } from "@material-ui/core";
 
 import { makeStyles } from "@material-ui/core/styles";
 
-const useStyles = makeStyles(() => ({
+const useStyles = makeStyles((theme) => ({
   avatar: {
     height: 20,
     width: 20,
+    // marginRight: theme.spacing(2),
+    // marginTop: theme.spacing(6),
     marginRight: 2,
     marginTop: 6,
   },
@@ -17,7 +19,9 @@ const SeenAvatar = (props) => {
   const { username, photoUrl } = props.otherUser;
 
   return (
+    <Box>
       <Avatar alt={username} src={photoUrl} className={classes.avatar} />
+    </Box>
   );
 };
 
