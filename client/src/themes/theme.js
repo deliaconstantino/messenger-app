fa24170ccd60a4dea1518c86ea2d8215fc595848
deliaconstantino <dconstan92@gmail.com>
2021-07-21 11:43:36 -0400

    removed custom breakpoints

--- client/src/themes/theme.js
+++ client/src/themes/theme.js
@@ -21,14 +21,4 @@ export const theme = createTheme({
     primary: { main: "#3A8DFF" },
     secondary: { main: "#B0B0B0" }
   },
-  // breakpoints: {
-  //   values: {
-  //     xs: 0,
-  //     sm: 600,
-  //     md: 960,
-  //     lg: 1280,
-  //     xl: 1920,
-  //     mdCustom: 850,
-  //   },
-  // },
 });
