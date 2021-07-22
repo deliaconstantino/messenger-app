119a28a82dfac4f635322bd48afedd6244a819cc
deliaconstantino <dconstan92@gmail.com>
2021-07-22 14:29:25 -0400

    render chat bubble on page

--- client/src/components/UserAccess/PhraseBox.js
+++ client/src/components/UserAccess/PhraseBox.js
@@ -3,10 +3,14 @@ import Box from "@material-ui/core/Box";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
+import Avatar from '@material-ui/core/Avatar';
 
 const useStyles = makeStyles((theme) => ({
   text: {
     ...theme.typography.phrase,
+  },
+  bubble: {
+
   }
 }));
 
@@ -14,6 +18,7 @@ const PhraseBox = () => {
   const classes = useStyles();
   return (
     <>
+      <Avatar alt="Remy Sharp" src="whiteBubble.png" className={classes.bubble} />
       <Typography item className={classes.text}>Converse with anyone</Typography>
       <Typography item className={classes.text}>with any language</Typography>
     </>
