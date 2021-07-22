9eaa6077bb5b2c51887cafc55683e0d9b1be5541
deliaconstantino <dconstan92@gmail.com>
2021-07-22 15:31:14 -0400

    formatted code

--- client/src/components/UserAccess/PhraseBox.js
+++ client/src/components/UserAccess/PhraseBox.js
@@ -3,24 +3,33 @@ import Box from "@material-ui/core/Box";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
-import Avatar from '@material-ui/core/Avatar';
+import Avatar from "@material-ui/core/Avatar";
 
 const useStyles = makeStyles((theme) => ({
   text: {
     ...theme.typography.phrase,
   },
   bubble: {
-    padding: theme.spacing(4)
-  }
+    padding: theme.spacing(4),
+  },
 }));
 
 const PhraseBox = () => {
   const classes = useStyles();
   return (
     <>
-      <Avatar item alt="chat bubble" src="whiteBubble.png" className={classes.bubble} />
-      <Typography item className={classes.text}>Converse with anyone</Typography>
-      <Typography item className={classes.text}>with any language</Typography>
+      <Avatar
+        item
+        alt="chat bubble"
+        src="whiteBubble.png"
+        className={classes.bubble}
+      />
+      <Typography item className={classes.text}>
+        Converse with anyone
+      </Typography>
+      <Typography item className={classes.text}>
+        with any language
+      </Typography>
     </>
   );
 };
