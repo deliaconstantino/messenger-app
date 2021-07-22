3ce80372501b7d4a1a064de4928c534451d8e3f3
deliaconstantino <dconstan92@gmail.com>
2021-07-22 19:28:28 -0400

    refactored makeStyles prop into separate file

--- client/src/components/UserAccess/UserAccess.js
+++ client/src/components/UserAccess/UserAccess.js
@@ -7,70 +7,13 @@ import Hidden from "@material-ui/core/Hidden";
 import Box from "@material-ui/core/Box";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
-import { makeStyles } from "@material-ui/core/styles";
 import { Redirect, useHistory } from "react-router-dom";
 import { connect } from "react-redux";
-
-const useStyles = makeStyles((theme) => ({
-  image: {
-    backgroundImage: "url(bg-img.png)",
-    backgroundRepeat: "no-repeat",
-    backgroundSize: "cover",
-    backgroundPosition: "center-top",
-    minHeight: "100vh",
-  },
-  backgroundColor: {
-    backgroundSize: "cover",
-    height: "100%",
-    backgroundImage: "linear-gradient(#3A8DFF 0%, #86B9FF 100%)",
-    opacity: "0.85",
-  },
-  buttonGrid: {
-    display: "flex",
-    margin: theme.spacing(1, -3),
-    flexDirection: "row",
-    justifyContent: "flex-end",
-    [theme.breakpoints.down("xs")]: {
-      margin: theme.spacing(1, -1),
-    },
-  },
-  text: {
-    ...theme.typography.loginSignup,
-    margin: theme.spacing(2, 2),
-  },
-
-  button: {
-    ...theme.typography.userAccessButton,
-    padding: theme.spacing(1, 6),
-    boxShadow: "1px 2px 10px #d3d3d3",
-    [theme.breakpoints.down("xs")]: {
-      padding: theme.spacing(1, 3),
-    },
-  },
-  phraseGrid: {
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    justifyContent: "center",
-    minHeight: "75vh",
-    position: "relative",
-  },
-  formContainer: {
-    display: "flex",
-    direction: "column",
-    justifyContent: "space-evenly",
-    alignItems: "center",
-    padding: theme.spacing(2),
-    [theme.breakpoints.down("xs")]: {
-      margin: theme.spacing(6, 0),
-    },
-    margin: theme.spacing(12, 0),
-  },
-}));
+import { useStylesUserAccess } from "./useStylesObject";
 
 function UserAccess(props) {
   const history = useHistory();
-  const classes = useStyles();
+  const classes = useStylesUserAccess();
 
   const [showLogin, updateShowLogin] = useState(props.showLogin);
   const [showSignup, updateShowSignup] = useState(props.showSignup);
--- client/src/components/UserAccess/useStylesObject.js
+++ client/src/components/UserAccess/useStylesObject.js
@@ -9,4 +9,59 @@ export const useStylesForm = makeStyles((theme) => ({
   },
 }));
 
-// export default useStyles;
+export const useStylesUserAccess = makeStyles((theme) => ({
+  image: {
+    backgroundImage: "url(bg-img.png)",
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
+    backgroundPosition: "center-top",
+    minHeight: "100vh",
+  },
+  backgroundColor: {
+    backgroundSize: "cover",
+    height: "100%",
+    backgroundImage: "linear-gradient(#3A8DFF 0%, #86B9FF 100%)",
+    opacity: "0.85",
+  },
+  buttonGrid: {
+    display: "flex",
+    margin: theme.spacing(1, -3),
+    flexDirection: "row",
+    justifyContent: "flex-end",
+    [theme.breakpoints.down("xs")]: {
+      margin: theme.spacing(1, -1),
+    },
+  },
+  text: {
+    ...theme.typography.loginSignup,
+    margin: theme.spacing(2, 2),
+  },
+
+  button: {
+    ...theme.typography.userAccessButton,
+    padding: theme.spacing(1, 6),
+    boxShadow: "1px 2px 10px #d3d3d3",
+    [theme.breakpoints.down("xs")]: {
+      padding: theme.spacing(1, 3),
+    },
+  },
+  phraseGrid: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    minHeight: "75vh",
+    position: "relative",
+  },
+  formContainer: {
+    display: "flex",
+    direction: "column",
+    justifyContent: "space-evenly",
+    alignItems: "center",
+    padding: theme.spacing(2),
+    [theme.breakpoints.down("xs")]: {
+      margin: theme.spacing(6, 0),
+    },
+    margin: theme.spacing(12, 0),
+  },
+}));
