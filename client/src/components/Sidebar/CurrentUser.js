94a7f1a07b15fbbc1fb5ea35f56bab7c96edfbff
deliaconstantino <dconstan92@gmail.com>
2021-07-23 11:46:49 -0400

    refactored Sidebar/CurrentUser to use useSelector hook

--- client/src/components/Sidebar/CurrentUser.js
+++ client/src/components/Sidebar/CurrentUser.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Box, Typography } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
-import { connect } from "react-redux";
+import { useSelector } from "react-redux";
 import { BadgeAvatar } from "./index";
 import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
 
@@ -32,10 +32,9 @@ const useStyles = makeStyles(() => ({
   },
 }));
 
-const CurrentUser = (props) => {
+const CurrentUser = () => {
   const classes = useStyles();
-
-  const user = props.user || {};
+  const user = useSelector(state => state.user) || {}
 
   return (
     <Box className={classes.root}>
@@ -48,10 +47,4 @@ const CurrentUser = (props) => {
   );
 };
 
-const mapStateToProps = (state) => {
-  return {
-    user: state.user,
-  };
-};
-
-export default connect(mapStateToProps)(CurrentUser);
+export default (CurrentUser);
