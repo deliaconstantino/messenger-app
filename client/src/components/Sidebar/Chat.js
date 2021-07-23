ee98c75588b1561d4e80be7585e094a9a19a9202
deliaconstantino <dconstan92@gmail.com>
2021-07-22 20:08:05 -0400

    removed unused Class component code

--- client/src/components/Sidebar/Chat.js
+++ client/src/components/Sidebar/Chat.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import { Box } from "@material-ui/core";
 import { BadgeAvatar, ChatContent } from "../Sidebar";
